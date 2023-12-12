bb149bfaa950985107843354a3b5e4b9775a3407
SereaAlex <alex.serea1156@gmail.com>
2023-12-12 15:01:44 +0200

    The changes that are now in use is: 1. Completing the help section 2. Auto-completing files that are in the directory 3. Changing the directory that you are from forwards and backwards to create, code, remove, and edit whatever you have in your distro

--- Source_File/myowntoolkit.py
+++ Source_File/myowntoolkit.py
@@ -3,7 +3,14 @@
 
 import os
 import time
+import readline
+import glob
 
+def completer(text, state):
+    return (glob.glob(text + '*') + [None])[state]
+
+readline.set_completer(completer)
+readline.parse_and_bind("tab: complete")
 
 def slow_print(text, delay=0.01):
     for char in text:
@@ -23,7 +30,7 @@ def home():
 ░░░██║░░░██║░░██║██║░░██║██║░░░░░█████═╝░██║░░░██║░░░  
 ░░░██║░░░██║░░██║██║░░██║██║░░░░░██╔═██╗░██║░░░██║░░░  
 ░░░██║░░░╚█████╔╝╚█████╔╝███████╗██║░╚██╗██║░░░██║░░░  
-░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═╝░░╚═╝╚═╝░░░╚═╝░░░  -v.0.3
+░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═╝░░╚═╝╚═╝░░░╚═╝░░░  -v.0.5
 
 ███████╗███╗░░██╗░░░░░██╗░█████╗░██╗░░░██╗███████╗██████╗░
 ██╔════╝████╗░██║░░░░░██║██╔══██╗╚██╗░██╔╝██╔════╝██╔══██╗
@@ -50,69 +57,21 @@ slow_print(home(),0.0001)
 
 program_on = 1
 
-
-
+#The program starts here
 
 while program_on:
     
     user_input = input("\nType:")
     
+#not enviroument commands
     
-    if user_input == "1" or user_input == "testing":
-    
-        files = os.listdir()
-        
-        py_files = [file for file in files if file.endswith('.py')]
-        
-        if len(py_files) == 1:
-            
-            print("No .py files found. Please add some .py files to test and try again.")
-        
-        else:
-            print("""What would you like to test?:""")
-            test_input = input("\nTest: ")
-            os.system(f"python3 {test_input}")
-            print("Test completed! (now back to the toolkit)")
-
-    
-    if user_input == "2" or user_input == "code" or user_input == "create":
-        
-        clear_screen()
-        print("Here you can create a file, edit a file or code!")
-        create_input = input("\nWhat do you choose?: ")
-        
-        if create_input == "file" or create_input == "directory":
-            
-            clear_screen()
-            choosing = input("\nFile or directory?: ")
-            
-            if choosing == "file":
-                
-                clear_screen()
-                name_file = input("\nWhat is the name of the file?: ")
-                os.system(f"touch {name_file}")
-                print(f"\nYour file named {name_file} was created!")
-                
-            elif choosing == "directory":
-                
-                clear_screen()
-                name_directory = input("What is the name of the directory?: ")
-                os.system(f"mkdir {name_directory}")
-                print(f"\nYour directory named {name_directory} was created!")
-        
-        if create_input == "code": 
-            
-            clear_screen()
-            code_input = input("What would you like to code?: ")
-            os.system(f"code {code_input}")
-    
-    if user_input == "home" or user_input == "4.b":
+    if user_input == "home":
         
         clear_screen()
         slow_print(home(),0.0001)
     
     
-    if user_input == "clear" or user_input == "4.a":
+    if user_input == "clear":
         
         clear_screen()
         
@@ -124,14 +83,20 @@ while program_on:
         
         os.system("pwd")
         
-    if user_input == "cd back":
+    if user_input == "cd ..":
         
-         os.system("cd ..")
+         os.chdir("..")
         
-    if user_input == "cd forward":
+    if user_input == "cd":
         
-         cd_input = input("\nWhere?: ")
-         os.system(f"cd {cd_input}")
+        try:
+            
+            cd_input = input("\nWhere?: ")
+            os.chdir(cd_input)
+    
+        except:
+            
+            print("Please enter the correct path name.")
     
     if user_input == "rm":
         
@@ -143,7 +108,7 @@ while program_on:
             os.system(f"rm {rm_file_input}")
             print(f"The file named {rm_file_input} was removed.")
         
-        if rm_input == "directory":
+        if rm_input == "directory" or rm_input == "dir":
             
             rm_directory_input = input("Name: ")
             os.system(f"sudo rm -r {rm_directory_input}")
@@ -158,10 +123,66 @@ while program_on:
         
         edit_input = input("Name: ")
         os.system(f"nano {edit_input}")
+
+#home commands
+
+    if user_input == "1" or user_input == "testing":
+    
+        files = os.listdir()
         
+        py_files = [file for file in files if file.endswith('.py')]
+        
+        if len(py_files) == 1:
+               
+         print("No .py files found. Please add some .py files to test and try again.")
+        
+        else:
+            print("""What would you like to test?:""")
+            test_input = input("\nTest: ")
+            os.system(f"python3 {test_input}")
+            print("Test completed! (now back to the toolkit)")
+
+    
+    if user_input == "2" or user_input == "code" or user_input == "create":
+        
+        clear_screen()
+        print("""Type "create" to create a file or directory, \n or type "code" to just code ^-^""")
+        
+        create_input = input("\nWhat do you choose?: ")
+        
+        if create_input == "create":
+            
+            clear_screen()
+            choosing = input("\nFile or directory?: ")
+            
+            if choosing == "file":
+                
+                clear_screen()
+                name_file = input("\nWhat is the name of the file?: ")
+                os.system(f"touch {name_file}")
+                print(f"\nYour file named {name_file} was created!")
+                
+            elif choosing == "directory" or choosing == "dir":
+                
+                clear_screen()
+                name_directory = input("What is the name of the directory?: ")
+                os.system(f"mkdir {name_directory}")
+                print(f"\nYour directory named {name_directory} was created!")
+        
+        if create_input == "code": 
+            
+            clear_screen()
+            code_input = input("What would you like to code?: ")
+            os.system(f"code {code_input}")
+            
+            finnish_input = input("Done coding? Amazing! Now press enter to go back to the toolkit...\n")
+    
+
     if user_input == "3":
         
         os.system("curl parrot.live")
+        
+        print("Not funny after a while ha? :))")
         
         
     if user_input == "4" or user_input == "help":
@@ -184,17 +205,19 @@ while program_on:
 ╚═════╝░╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝
 
 
-[4.a] Type "clear" whenever your screen feels too crowded
-[4.b] Type "home" to go back to the original screen
-[4.c] Type "create" to create a file or a direcotry
-[4.d] Type "code" in the create section to code stuff!
-[4.e] Type "rm" to remove a directory or a file
-[4.f] Type "pwd" to check the location that you are
-[4.g] Type "ls" to check the contents of the directory that you are
-[4.h] Type "cat" to read the contents of the file/s that you have
-[4.i] Type "testing" for easy acces to test your programs (for now, only python) 
-
-(If you wonder why there is no "cd", \n it's because it doesn't work for now -.-")
+[4.1] Type "clear" whenever your screen feels too crowded
+[4.2] Type "home" to go back to the original screen
+[4.3] Type "create" to create a file or a direcotry
+[4.4] Type "code" in the create section to code stuff!
+[4.5] Type "rm" to remove a directory or a file
+[4.6] Type "pwd" to check the location that you are
+[4.7] Type "ls" to check the contents of the directory that you are
+[4.8] Type "cat" to read the contents of the file/s that you have
+[4.9] Type "testing" for easy acces to test your programs (for now, only python) 
+[4.10]Type "cd" to change directories forward only
+[4.11]Type "cd .." to change directories backwards
+[4.12]Type "edit" to change or rewrite files that you want directly in the toolkit
+P.S: You can use " CTRL-C" for any operation to close
               """,0.0001)
         
         
