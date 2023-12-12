36645668b44d79356e32f26c4d80b1c47a6e6fdd
SereaAlex <alex.serea1156@gmail.com>
2023-12-12 10:49:04 +0200

    .

--- Source_File/myowntoolkit.py
+++ Source_File/myowntoolkit.py
@@ -56,12 +56,23 @@ program_on = 1
 while program_on:
     
     user_input = input("\nType:")
+    
+    
     if user_input == "1" or user_input == "testing":
+    
+        files = os.listdir()
         
-        print("""What would you like to test?:""")
-        test_input = input("\nTest: ")
-        os.system(f"python3 {test_input}")
-        print("Test completed! (now back to the toolkit)")
+        py_files = [file for file in files if file.endswith('.py')]
+        
+        if len(py_files) == 1:
+            
+            print("No .py files found. Please add some .py files to test and try again.")
+        
+        else:
+            print("""What would you like to test?:""")
+            test_input = input("\nTest: ")
+            os.system(f"python3 {test_input}")
+            print("Test completed! (now back to the toolkit)")
 
     
     if user_input == "2" or user_input == "code" or user_input == "create":
@@ -70,7 +81,7 @@ while program_on:
         print("Here you can create a file, edit a file or code!")
         create_input = input("\nWhat do you choose?: ")
         
-        if create_input == "create":
+        if create_input == "file" or create_input == "directory":
             
             clear_screen()
             choosing = input("\nFile or directory?: ")
@@ -180,6 +191,8 @@ while program_on:
 [4.e] Type "rm" to remove a directory or a file
 [4.f] Type "pwd" to check the location that you are
 [4.g] Type "ls" to check the contents of the directory that you are
+[4.h] Type "cat" to read the contents of the file/s that you have
+[4.i] Type "testing" for easy acces to test your programs (for now, only python)
 
 (If you wonder why there is no "cd", \n it's because it doesn't work for now -.-")
               """,0.0001)
